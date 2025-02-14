d45e0d87acc372bf041f676cba427eda0afe057a
SaraiNoQ <2994498974@qq.com>
2025-02-14 10:07:42 +0800

    style: Reactive and Responsive layout

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,10 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  reactStrictMode: true,
+  env: {
+    PORT: "3000",
+  },
 };
 
 export default nextConfig;
--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,17 +1,23 @@
 import { ThemeProvider } from 'next-themes';
 import { BookmarkProvider } from '../contexts/BookmarkContext';
 import { CategoryProvider } from '../contexts/CategoryContext';
+import Head from 'next/head';
 import '../styles/globals.css';
 import type { AppProps } from 'next/app';
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
-    <ThemeProvider attribute="class">
-      <CategoryProvider>
-        <BookmarkProvider>
-          <Component {...pageProps} />
-        </BookmarkProvider>
-      </CategoryProvider>
-    </ThemeProvider>
+    <>
+      <Head>
+        <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=0" />
+      </Head>
+      <ThemeProvider attribute="class">
+        <CategoryProvider>
+          <BookmarkProvider>
+            <Component {...pageProps} />
+          </BookmarkProvider>
+        </CategoryProvider>
+      </ThemeProvider>
+    </>
   );
-} 
+}
